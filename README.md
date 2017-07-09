949d1625dab6f1c88cf8c7f10bdf1ff606e814b4
Josh Morel <morel.josh@hotmail.com>
2017-07-09 11:51:09 -0400

    Use geo (#2)
    * Added geolocation usage and finished app
    
    * Updated readme

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# Local Weather App
+# Random Quote Machine
 
-Completion of Local Weather App freeCodeCamp challenge
+Completion of Local Weather Free Code Camp challenge utilizing:
 
-- [ ] Get user's location (lat, long)
-- [ ] If not available provide option to enter address 
-- [ ] With location, get current weather from https://darksky.net/dev/docs/forecast
-- [ ] asdfasdf
+* Vanilla JS
+* Browser Geolocation
+* CSS3 FlexBox
+* [Yahoo Weather API](https://developer.yahoo.com/weather/)
+* [Unsplash](https://unsplash.com/) for photos
+
+Available for [view on CodePen](https://codepen.io/joshmorel/pen/KqxXqg).
+
+## User Stories
+
+- [x] I can see the weather in my current location.
+- [x] I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+- [x] User Story: I can push a button to toggle between Fahrenheit and Celsius.
--- app.js
+++ app.js
@@ -1,258 +1,211 @@
-"use strict";
-function fetchJsonp(_url, options = {}) {
 
-  const defaultOptions = {
-    timeout: 5000,
-    jsonpCallback: 'callback',
-    jsonpCallbackFunction: null
-  };
+const localWeather =
 
-  function generateCallbackFunction() {
-    return `jsonp_${Date.now()}_${Math.ceil(Math.random() * 100000)}`;
-  }
+(function IIFE(document) {
+  "use strict";
 
-  function removeScript(scriptId) {
-    const script = document.getElementById(scriptId);
-    if (script) {
-      document.getElementsByTagName('head')[0].removeChild(script);
+  function displayImage(conditionCode) {
+
+    const codeNum = parseInt(conditionCode, 10);
+    const baseUrl = 'https://source.unsplash.com/';
+    const defaultImageId = {id: '3oyeaivM_fE', userName: '@qstevenson', fullName: 'Quin Stevenson'};
+    const bgImg = document.querySelector('.bg-img');
+    const creditBadge = document.querySelector('.unsplash-badge');
+    const creditName = document.querySelector('.unsplash-name');
+    const toLoad = document.querySelectorAll('.to-load');
+
+    const image = imgFromCode(codeNum);
+    bgImg.style.backgroundImage = `url(${baseUrl}${image.id})`;
+    creditBadge.setAttribute('href', `http://unsplash.com/${image.userName}?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge`);
+    creditBadge.setAttribute('title', `Download free do whatever you want high-resolution photos from ${image.fullName}`);
+    creditName.innerText = image.fullName;
+
+    toLoad.forEach(el => el.style.visibility = "visible");
+
+    function imgFromCode(conditionCode) {
+      switch (conditionCode) {
+        case 0: //'tornado'
+          return {id: '8eFbe3jQZ7Y', userName: '@tulen', fullName: 'Tulen Travel'};
+          break;
+        case 1: //tropical storm
+        case 2: //hurricane
+        case 3: //severe thunderstorms
+        case 4: // thunderstorms
+          return {id: 'jh2KTqHLMjE', userName: '@jeremythomasphoto', fullName: 'Jeremy Thomas'};
+          break;
+        case 5: //	mixed rain and snow
+        case 6: //	mixed rain and sleet
+        case 7: //	mixed snow and sleet
+        case 18: //	sleet
+          return {id: 'T7RlFC8dH60', userName: '@lolaguti', fullName: 'Lola Guti'};
+          break;
+        case 8: //	freezing drizzle
+        case 10: //	freezing rain
+          return {id: 'DqGsjytXOXg', userName: '@andy_walton', fullName: 'Andrew Walton'};
+          break;
+        case 9: //	drizzle
+        case 11: // showers
+        case 12: //	showers
+        case 39: //	scattered thunderstorms
+        case 40: //	scattered showers
+          return {id: 'Nw_D8v79PM4', userName: '@r_shayesrehpour', fullName: 'reza shayestehpour'};
+          break;
+        case 17: //	hail
+        case 35: //	mixed rain and hail
+          return {id: 'F-t5EpfQNpk', userName: '@eutahm', fullName: 'Eutah Mizushima'};
+          break;
+        case 19: //	dust
+          return {id: 'wdMWMHXUpsc', userName: '@sickle', fullName: 'Sergey Pesterev'};
+          break;
+        case 20: //	foggy
+          return {id: 'IOJE_IE1h-A', userName: '@amundroed', fullName: 'Amund Røed'};
+          break;
+        case 21: //	haze
+        case 22: //	smoky
+          return {id: 'jYChcwbXqnI', userName: '@t_g_m_l', fullName: 'Thomas Morter-Laing'};
+          break;
+        case 15: //blowing snow
+        case 23: //	blustery
+        case 24: //	windy
+          return {id: 'XHZhI_Zy6TM', userName: '@lesanderson', fullName: 'Les Anderson'};
+          break;
+        case 25: //	cold
+          return {id: 'Gw31jigR9j4', userName: '@robertbye', fullName: 'Rob Bye'};
+          break;
+        case 26: //	cloudy
+        case 27: //	mostly cloudy (night)
+        case 28: //	mostly cloudy (day)
+          return {id: 'V4qjYCac7y8', userName: '@atronyx', fullName: 'Artem Anokhin'};
+          break;
+        case 29: //	partly cloudy (night)
+          return {id: 'g3QBQto9Jt0', userName: '@matthewkane', fullName: 'Matthew Kane'};
+          break;
+        case 30: //	partly cloudy (day)
+        case 44: //	partly cloudy
+          return {id: 'BTubi6qaY6Q', userName: '@aaronburden', fullName: 'Aaron Burden'};
+          break;
+        case 31: //	clear (night)
+          return {id: 'tqzqzH8hb5A', userName: '@sveninho', fullName: 'Sven Scheuermeier'};
+          break;
+        case 32: //	sunny
+        case 34: //	fair (day)
+          return {id: 'u7ldh_tgH3s', userName: '@dakotaroosphotography', fullName: 'Dakota Roos'};
+          break;
+        case 33: //	fair (night)
+          return {id: '2uEqc-9P3oQ', userName: '@jeremythomasphoto', fullName: 'Jeremy Thomas'};
+          break;
+        case 36: //	hot
+          return {id: 'qQfb5WBDB5o', userName: '@adriencoquet', fullName: 'Adrien Coquet'};
+          break;
+        case 37: //	isolated thunderstorms
+        case 38: //	scattered thunderstorms
+        case 45: //	thundershowers
+        case 47: //	isolated thundershowers
+          return {id: 'uxwKpUvwGuY', userName: '@jayd', fullName: 'Jay Dantinne'};
+          break;
+        case 13: //	snow flurries
+        case 14: //	light snow showers
+        case 16: //	snow
+        case 41: //	heavy snow
+        case 42: //	scattered snow showers
+        case 43: //	heavy snow
+        case 46: //	snow showers
+          return {id: '-iK6ZxQC6EU', userName: '@markrabephotography', fullName: 'Mark Rabe'};
+          break;
+        case 3200: //	not available
+        default:
+          return defaultImageId;
+          break;
+      }
     }
   }
 
-  // to avoid param reassign
-  let url = _url;
-  const timeout = options.timeout || defaultOptions.timeout;
-  const jsonpCallback = options.jsonpCallback || defaultOptions.jsonpCallback;
-
-  let timeoutId;
-
-  return new Promise((resolve, reject) => {
-    const callbackFunction = options.jsonpCallbackFunction || generateCallbackFunction();
-    const scriptId = `${jsonpCallback}_${callbackFunction}`;
-
-    window[callbackFunction] = (response) => {
-      resolve({
-        ok: true,
-        // keep consistent with fetch API
-        json: () => Promise.resolve(response),
-      });
-
-      if (timeoutId) clearTimeout(timeoutId);
-
-      removeScript(scriptId);
-    };
-
-    // Check if the user set their own params, and if not add a ? to start a list of params
-    url += (url.indexOf('?') === -1) ? '?' : '&';
-
-    const jsonpScript = document.createElement('script');
-    jsonpScript.setAttribute('src', `${url}${jsonpCallback}=${callbackFunction}`);
-    if (options.charset) {
-      jsonpScript.setAttribute('charset', options.charset);
-    }
-    jsonpScript.id = scriptId;
-    document.getElementsByTagName('head')[0].appendChild(jsonpScript);
-
-    timeoutId = setTimeout(() => {
-      reject(new Error(`JSONP request to ${_url} timed out`));
-
-      removeScript(scriptId);
-    }, timeout);
-
-    // Caught if got 404/500
-    jsonpScript.onerror = () => {
-      reject(new Error(`JSONP request to ${_url} failed`));
-
-      removeScript(scriptId);
-      if (timeoutId) clearTimeout(timeoutId);
-    };
-  });
-}
-
-function displayImage(conditionCode) {
-
-  const codeNum = parseInt(conditionCode, 10);
-  const baseUrl = 'https://source.unsplash.com/';
-  const defaultImageId = {id: '3oyeaivM_fE', userName: '@qstevenson', fullName: 'Quin Stevenson'};
-
-  const background = document.querySelector('.flex-container');
-  const creditBadge = document.querySelector('.credit-badge');
-  const creditName = document.querySelector('.credit-name');
-
-  const image = imgFromCode(codeNum);
-  background.style.backgroundImage = `url(${baseUrl}${image.id})`;
-  creditBadge.setAttribute('href', `http://unsplash.com/${image.userName}?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge`);
-  creditBadge.setAttribute('title', `Download free do whatever you want high-resolution photos from ${image.fullName}`);
-  creditName.innerText = image.fullName;
-
-  // creditBadge.innerHTML = ``;
-  // if (!conditionCode) {
-  //   return baseUrl + defaultImageId;
-  // }
-  // return baseUrl + idFromCode(conditionCode);
-
-  function imgFromCode(conditionCode) {
-    switch (conditionCode) {
-      case 0: //'tornado'
-        return {id: '8eFbe3jQZ7Y', userName: '@tulen', fullName: 'Tulen Travel'};
-        break;
-      case 1: //tropical storm
-      case 2: //hurricane
-      case 3: //severe thunderstorms
-      case 4: // thunderstorms
-        return {id: 'jh2KTqHLMjE', userName: '@jeremythomasphoto', fullName: 'Jeremy Thomas'};
-        break;
-      case 5: //	mixed rain and snow
-      case 6: //	mixed rain and sleet
-      case 7: //	mixed snow and sleet
-      case 18: //	sleet
-        return {id: 'T7RlFC8dH60', userName: '@lolaguti', fullName: 'Lola Guti'};
-        break;
-      case 8: //	freezing drizzle
-      case 10: //	freezing rain
-        return {id: 'DqGsjytXOXg', userName: '@andy_walton', fullName: 'Andrew Walton'};
-        break;
-      case 9: //	drizzle
-      case 11: // showers
-      case 12: //	showers
-      case 39: //	scattered thunderstorms
-      case 40: //	scattered showers
-        return {id: 'Nw_D8v79PM4', userName: '@r_shayesrehpour', fullName: 'reza shayestehpour'};
-        break;
-      case 17: //	hail
-      case 35: //	mixed rain and hail
-        return {id: 'F-t5EpfQNpk', userName: '@eutahm', fullName: 'Eutah Mizushima'};
-        break;
-      case 19: //	dust
-        return {id: 'wdMWMHXUpsc', userName: '@sickle', fullName: 'Sergey Pesterev'};
-        break;
-      case 20: //	foggy
-        return {id: '-pCHz5XiMb0', userName: '@atn', fullName: 'Etienne Desclides'};
-        break;
-      case 21: //	haze
-      case 22: //	smoky
-        return {id: 't1mqA3V3-7g', userName: '@florianklauer', fullName: 'Florian Klauer'};
-        break;
-      case 23: //	blustery
-      case 24: //	windy
-        return {id: 'XHZhI_Zy6TM', userName: '@lesanderson', fullName: 'Les Anderson'};
-        break;
-      case 25: //	cold
-        return {id: 'Gw31jigR9j4', userName: '@robertbye', fullName: 'Rob Bye'};
-        break;
-      case 26: //	cloudy
-      case 27: //	mostly cloudy (night)
-      case 28: //	mostly cloudy (day)
-        return {id: '7caumHzcqL4', userName: '@chuchad', fullName: 'Vladimir Chuchadeev'};
-        break;
-      case 29: //	partly cloudy (night)
-        return {id: 'g3QBQto9Jt0', userName: '@matthewkane', fullName: 'Matthew Kane'};
-        break;
-      case 30: //	partly cloudy (day)
-      case 44: //	partly cloudy
-        return {id: 'AaJ2TuDS1uo', userName: '@photalife', fullName: 'Darren Coleshill'};
-        break;
-      case 31: //	clear (night)
-        return {id: 'tqzqzH8hb5A', userName: '@sveninho', fullName: 'Sven Scheuermeier'};
-        break;
-      case 32: //	sunny
-        return {id: 'u7ldh_tgH3s', userName: '@dakotaroosphotography', fullName: 'Dakota Roos'};
-        break;
-      case 33: //	fair (night)
-        return {id: '2uEqc-9P3oQ', userName: '@jeremythomasphoto', fullName: 'Jeremy Thomas'};
-        break;
-      case 34: //	fair (day)
-        return {id: 'E9aetBe2w40', userName: '@sam', fullName: 'Sam Schooler'};
-        break;
-      case 36: //	hot
-        return {id: 'qQfb5WBDB5o', userName: '@adriencoquet', fullName: 'Adrien Coquet'};
-        break;
-      case 37: //	isolated thunderstorms
-      case 38: //	scattered thunderstorms
-      case 45: //	thundershowers
-      case 47: //	isolated thundershowers
-        return {id: 'uxwKpUvwGuY', userName: '@jayd', fullName: 'Jay Dantinne'};
-        break;
-      case 13: //	snow flurries
-      case 14: //	light snow showers
-      case 15:  //	blowing snow
-      case 16: //	snow
-      case 41: //	heavy snow
-      case 42: //	scattered snow showers
-      case 43: //	heavy snow
-      case 46: //	snow showers
-        return {id: 'Mexn4TZSlX4', userName: '@noahsilliman', fullName: 'Noah Silliman'};
-        break;
-      case 3200: //	not available
-      default:
-        return defaultImageId;
-        break;
-    }
-  }
-}
-
-function handleFailure() {
-  const weatherCard = document.querySelector('.weather-card');
-  weatherCard.innerText = "Sorry we couldn't retrieve the weather";
-  displayImage(); //display default image
-}
-
-function getLocationFromIP(ip) {
-  let endpoint = "http://ipinfo.io/";
-  if (ip) {
-    endpoint += ip + '/';
-  }
-  endpoint += 'json?';
-  return fetchJsonp(endpoint)
-    .then(blob => blob.json())
-    .catch((err) => {
-      throw new Error('getLocationFromIP -> ' + err.message);
-    });
-}
-
-function getWeatherFromCity(location) {
-  const endpoint = `https://uery.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where u = "c" and woeid in (select woeid from geo.places(1) where text='${location}')&format=json`;
-  return fetchJsonp(endpoint)
-    .then(blob => blob.json())
-    .then(data => Promise.resolve(data.query.results.channel.item.condition))
-    .catch((err) => {
-      throw new Error('getWeatherFromCity -> ' + err.message);
-    });
-}
-
-function getLocaleData() {
-  return getLocationFromIP().then(location => {
-    const city = `${location.city} ${location.region} ${location.country}`;
-    return getWeatherFromCity(city)
-      .then(weather => Object.assign({}, location, weather))
-      .catch((err) => {
-        throw new Error('getLocaleData -> ' + err.message);
-      });
-  })
-}
-
-function renderWeatherData(weather) {
-  const temperatureDiv = document.querySelector('.temperature');
-  const conditionsDiv = document.querySelector('.condition');
-  const units = "C";
-  temperatureDiv.innerHTML = `${weather.temp}&deg;${units}`;
-  conditionsDiv.innerText = weather.text;
-}
-
-function loadWeatherPage() {
-  getLocaleData().then(data => {
-    // console.log(data);
-    renderWeatherData(data);
-    displayImage(data.code);
-  }).catch((err) => {
+  function handleFailure(err) {
     console.log(err.message);
-    handleFailure();
-  });
-}
+    const weatherText = document.querySelector('.weather-text');
+    weatherText.innerText = "Sorry we couldn't retrieve the weather";
+  }
 
-function convertTemperature(degreesCelsius, convertToFahrenheit) {
-  const temperatureDiv = document.querySelector('#temperature');
-  const degrees = convertToFahrenheit ? (degreesCelsius * 9 / 5) + 32 : degreesCelsius;
-  const displayUnits = convertToFahrenheit ? 'F' : 'C';
-  temperatureDiv.innerHTML = `${degrees}&deg;${displayUnits}`;
-}
+  function getWeatherFromLocation() {
 
+    return new Promise((resolve, reject) => {
+
+      function getWeatherFromPosition(position) {
+        const crd = position.coords;
+        const yqlSubQuery = `SELECT woeid FROM geo.places(1) WHERE text="(${crd.latitude},${crd.longitude})"`;
+        const endpoint = `https://query.yahooapis.com/v1/public/yql?q=SELECT location, item.condition FROM weather.forecast WHERE u = "c" AND woeid IN (${yqlSubQuery})&format=json`;
+        fetch(endpoint)
+          .then(blob => blob.json())
+          .then(data => {
+            if(data.error) {
+              reject(new Error(data.error.description));
+            }
+            resolve(data.query.results.channel);
+          })
+          .catch((err) => reject(err));
+      }
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          getWeatherFromPosition,
+          (err) => reject(err),
+          {enableHighAccuracy: true, timeout: 5000, maximumAge: 0}
+        );
+      } else {
+        reject(new Error("Geolocation not supported"));
+      }
+
+    });
+  }
+
+  function renderWeatherData(weather, useFahrenheit) {
+    const condition = weather.item.condition;
+    const conditionsDiv = document.querySelector('.condition');
+    const temperatureDiv = document.querySelector('.temperature > .value');
+    const unitsDiv = document.querySelector('.active-units');
+    const locationDiv = document.querySelector('.location');
+
+    const units = useFahrenheit ? "F" : "C";
+    conditionsDiv.innerText = condition.text;
+    temperatureDiv.innerText = condition.temp;
+    unitsDiv.innerHTML = `&deg;${units}`;
+    locationDiv.innerText = weather.location.city;
+  }
+
+  function loadWeatherPage() {
+    getWeatherFromLocation().then(data => {
+      renderWeatherData(data);
+      displayImage(data.item.condition.code);
+    }).catch((err) => {
+      handleFailure(err);
+    });
+  }
+
+  function convertTemperature() {
+    const temperatureDiv = document.querySelector('.temperature > .value');
+    const currentTemp = parseInt(temperatureDiv.innerText,10);
+    const activeUnits = document.querySelector('.units > .active-units');
+    const inactiveUnits = document.querySelector('.units > .btn-convert-units');
+
+    if (!currentTemp) return;
+
+    if (inactiveUnits.innerText === 'F') {
+      temperatureDiv.innerText = Math.round((currentTemp * 9 / 5) + 32);
+      activeUnits.innerHTML = '&deg;F';
+      inactiveUnits.innerText = 'C';
+    } else if (inactiveUnits.innerText === 'C') {
+      temperatureDiv.innerText = Math.round((currentTemp - 32)*5/9);
+      activeUnits.innerHTML = '&deg;C';
+      inactiveUnits.innerText = 'F';
+    }
+  }
+
+  return {
+    loadWeatherPage: loadWeatherPage,
+    convertTemperature: convertTemperature
+  }
+
+})(window.document);
+
+//load weather on load of page
+localWeather.loadWeatherPage();
